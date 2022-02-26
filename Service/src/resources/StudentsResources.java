037266ebe98575a78b545ae869bccdcd7301db45
Mrbh <Mrbhmr@gmail.com>
2022-02-26 19:55:19 +0100

    Add StudentsResponse class to service

---
+++ Service/src/resources/StudentsResources.java
@@ -0,0 +1,21 @@
+package resources;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.awt.*;
+import java.util.ResourceBundle;
+
+@Path("/students")
+public class StudentsResources {
+    @GET //GET at http://localhost:XXXX/students/hello
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sayHello(){
+        String msg = "Hello, your service works!";
+        return Response.status(Response.Status.OK).entity(msg).build();
+        // return Response.ok(msg).build(); This is another way to create a 200 ok response
+    }
+}
