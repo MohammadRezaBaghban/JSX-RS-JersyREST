f0d2b5c2ecd3eb12dda8ffe1bee81c29e1692e40
Mrbh <Mrbhmr@gmail.com>
2022-02-27 15:39:16 +0100

    Add FakeStudentRepository

---
+++ Service/src/repository/FakeStudentRepository.java
@@ -0,0 +1,36 @@
+package repository;
+
+import model.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class FakeStudentRepository extends StudentRepository{
+
+    private final List<Student> students = new ArrayList<>();
+
+    public FakeStudentRepository(){
+        students.add(new Student(1,"Joe smith"));
+        students.add(new Student(2,"Ann Johnsson"));
+        students.add(new Student(3,"Jnn Pereson"));
+        students.add(new Student(4,"Mohammad Baghban"));
+        students.add(new Student(5,"Miranda Winslet"));
+    }
+
+    @Override
+    public int count() { return students.size(); }
+
+    @Override
+    public Collection<Student> getAll() { return students; }
+
+    @Override
+    public Student get(int nr) {
+        return null;
+    }
+
+    @Override
+    public List<Student> filter(String name) {
+        return students;
+    }
+}
