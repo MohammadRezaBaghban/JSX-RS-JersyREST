048cd870ff88ad59d18f0bcaab5694fbc871b404
Mrbh <Mrbhmr@gmail.com>
2022-03-17 17:49:58 +0100

    Implement filter function of AuthenticationFilter + isValidUser

--- Service/src/main/java/AuthenticationFilter.java
+++ Service/src/main/java/AuthenticationFilter.java
@@ -2,7 +2,12 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.*;
+
+import static javax.ws.rs.client.Entity.entity;
 
 public class AuthenticationFilter implements ContainerRequestFilter {
 
@@ -11,12 +16,60 @@ public class AuthenticationFilter implements ContainerRequestFilter {
      * - resourceInfo will be assigned/set automatically by the Jersey framework, you do not need to assign/set it.
      */
 
+    private static Map<String, String> credentials = new HashMap<String, String>();
+
+    AuthenticationFilter() {
+        credentials.put("mrbhmr@gmail.com", "1234");
+    }
+
+
     @Context
     private ResourceInfo resourceInfo;
 
     // requestContext contains information about the HTTP request message
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        final String AUTHORIZATION_PROPERTY = "Authorization";
+        final String AUTHENTICATION_SCHEME = "Basic";
+
+        // Get Response Header
+        final MultivaluedMap<String, String> headers = requestContext.getHeaders();
+
+        // Fetch Authorization Header
+        final List<String> authorization = headers.get("AUTHORIZATION_PROPERTY");
+
+        if (authorization == null || authorization.isEmpty()) {
+            Response response = Response
+                    .status(Response.Status.UNAUTHORIZED)
+                    .entity("Missing username and/or password.")
+                    .build();
+            requestContext.abortWith(response);
+            return;
+        }
+
+        // Get encoded username and password
+        final String encodedCredentials = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + "", "");
+
+        //Decode username and password into one string
+        String credentials = new String(Base64.getDecoder().decode(encodedCredentials.getBytes());
+
+        // Split username and password into one string
+        final StringTokenizer tokenizer = new StringTokenizer(credentials, ":");
+        final String username = tokenizer.nextToken();
+        final String password = tokenizer.nextToken();
+
+        // Check if username and password are valid (e.g., database)
+        // If not valid: abort with UNAUTHORIZED and stop
+        if (!isValidUser(username, password)) {
+            Response response = Response
+                    .status(Response.Status.UNAUTHORIZED)
+                    .entity("Inavlid username and/or password.")
+                    .build();
+            requestContext.abortWith(response);
+            return;
+        }
+
         // Here we will perform Authentication and authorization
         /* if we want to abort HTTP request, we do this:
             Response response  = Response.status(Response.status.UNAUTHORIZED).build();
@@ -33,5 +86,14 @@ public class AuthenticationFilter implements ContainerRequestFilter {
         // 2. check if the user has one these roles
         // 3. if not, abort requestContext with FROBIDDEN response
     }
+
+    private boolean isValidUser(String username, String password) {
+        if (credentials.containsKey(username)) {
+            var credentialPassword = credentials.get(username);
+            return credentialPassword.equals(password);
+        }
+        return false;
+    }
+
 
 }
